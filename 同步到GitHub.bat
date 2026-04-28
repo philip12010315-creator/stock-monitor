0eab5be8d26881479d96acb47e3fb540da2cf7b8
philip <philip@example.com>
2026-04-28 17:42:44 +0800

    Auto-sync: update UI and documentation

--- 同步到GitHub.bat
+++ 同步到GitHub.bat
@@ -2,9 +2,7 @@
 cd /d "%~dp0"
 
 echo [1/3] Force staging files...
-git add index.html
-git add data.json
-git add .gitignore
+git add .
 
 echo [2/3] Committing changes...
 git commit -m "Update data and index: %date% %time%"
